4f9e1ef7ca814250e9ffacd70fad8d0d9be2a579
Pablo <pablo@iconecta.es>
2022-04-04 10:33:30 +0200

    Migrate to React v18.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
-ReactDOM.render(
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root");
+
+if (!container) throw new Error("Could not find root element with id 'root'");
+
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
